f472b2cefb97cc4c53b4b5854b683eadf188602e
John Hoover <defvayne23@gmail.com>
2016-08-30 14:45:22 -0500

    Look for data attribute to skip validation on form.
    This adds no events to the form.
    
    Sovles #1.

--- ValidInput.js
+++ ValidInput.js
@@ -9,188 +9,191 @@
         form_submit,
         form_submits = form.querySelectorAll('.submit button');
 
-    // Add click event that sets the form_submit of this form
-    form.addEventListener('click', function(e) {
-      if(e.target.nodeName === 'BUTTON' || (e.target.nodeName === 'INPUT' && (e.target.getAttribute('type') === 'submit' || e.target.getAttribute('type') === 'image'))) {
-        form_submitted = this;
-        form_submit = e.target;
-      }
-    });
+    // Check for data attribute to skip validation.
+    if(!form.hasAttribute('data-validation-ignore')) {
+      // Add click event that sets the form_submit of this form
+      form.addEventListener('click', function(e) {
+        if(e.target.nodeName === 'BUTTON' || (e.target.nodeName === 'INPUT' && (e.target.getAttribute('type') === 'submit' || e.target.getAttribute('type') === 'image'))) {
+          form_submitted = this;
+          form_submit = e.target;
+        }
+      });
 
-    form.addEventListener('submit', function(e) {
-      var form_error = false,
-          required_fields = form.querySelectorAll('.required'),
-          submit_value = '';
+      form.addEventListener('submit', function(e) {
+        var form_error = false,
+            required_fields = form.querySelectorAll('.required'),
+            submit_value = '';
 
-      // This is set from another event that may not fire first
-      if(form_submit) {
-        // If last submit click is this form.
-        if(form_submitted === this) {
-          submit_value = form_submit.value;
+        // This is set from another event that may not fire first
+        if(form_submit) {
+          // If last submit click is this form.
+          if(form_submitted === this) {
+            submit_value = form_submit.value;
 
-          // Check for data attribute to skip validation.
-          if(form_submit.hasAttribute('data-validation-ignore')) {
-            return true;
+            // Check for data attribute to skip validation.
+            if(form_submit.hasAttribute('data-validation-ignore')) {
+              return true;
+            }
           }
         }
-      }
 
-      if(submit_value !== 'Back') {
-        Array.prototype.forEach.call(required_fields, function(field) {
-          var skip = false;
+        if(submit_value !== 'Back') {
+          Array.prototype.forEach.call(required_fields, function(field) {
+            var skip = false;
 
-          if(field.nodeName === 'INPUT' || field.nodeName === 'TEXTAREA') {
-            // Check if we should skip this input because it requires
-            // another input to have a value or a specific value
-            if(field.hasAttribute('data-requires')) {
-              var field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]:checked');
+            if(field.nodeName === 'INPUT' || field.nodeName === 'TEXTAREA') {
+              // Check if we should skip this input because it requires
+              // another input to have a value or a specific value
+              if(field.hasAttribute('data-requires')) {
+                var field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]:checked');
 
-              if(!field_requires) {
-                field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]');
-              }
+                if(!field_requires) {
+                  field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]');
+                }
 
-              if(field_requires) {
-                if(field.hasAttribute('data-requires-value')) {
-                  // If value is not what we expect, skip validation
-                  if(field_requires.value !== field.getAttribute('data-requires-value')) {
+                if(field_requires) {
+                  if(field.hasAttribute('data-requires-value')) {
+                    // If value is not what we expect, skip validation
+                    if(field_requires.value !== field.getAttribute('data-requires-value')) {
+                      skip = true;
+                    }
+                  } else if(field_requires.value === '') {
                     skip = true;
                   }
-                } else if(field_requires.value === '') {
-                  skip = true;
                 }
               }
-            }
 
-            if(skip === false) {
-              // Run validation on input
-              var field_error = input_check(form, field);
-
-              if(field_error === true) {
-                form_error = true;
-              }
-
-              field.addEventListener('blur', function(e) {
-                var field_error = input_check(form, this);
+              if(skip === false) {
+                // Run validation on input
+                var field_error = input_check(form, field);
 
                 if(field_error === true) {
                   form_error = true;
                 }
-              });
-            }
-          } else if(field.nodeName === 'SELECT') {
-            // Check if we should skip this input because it requires
-            // another input to have a value or a specific value
-            if(field.hasAttribute('data-requires')) {
-              var field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]:checked');
 
-              if(!field_requires) {
-                field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]');
+                field.addEventListener('blur', function(e) {
+                  var field_error = input_check(form, this);
+
+                  if(field_error === true) {
+                    form_error = true;
+                  }
+                });
               }
+            } else if(field.nodeName === 'SELECT') {
+              // Check if we should skip this input because it requires
+              // another input to have a value or a specific value
+              if(field.hasAttribute('data-requires')) {
+                var field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]:checked');
 
-              if(field_requires) {
-                if(field.hasAttribute('data-requires-value')) {
-                  // If value is not what we expect, skip validation
-                  if(field_requires.value !== field.getAttribute('data-requires-value')) {
+                if(!field_requires) {
+                  field_requires = form.querySelector('[name="'+field.getAttribute('data-requires')+'"]');
+                }
+
+                if(field_requires) {
+                  if(field.hasAttribute('data-requires-value')) {
+                    // If value is not what we expect, skip validation
+                    if(field_requires.value !== field.getAttribute('data-requires-value')) {
+                      skip = true;
+                    }
+                  } else if(field_requires.value === '') {
                     skip = true;
                   }
-                } else if(field_requires.value === '') {
+                }
+              }
+
+              if(skip === false) {
+                if(field.value === '') {
+                  form_error = true;
+                  if(field.getAttribute('data-required-label')) {
+                    form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
+                  } else {
+                    field.parentNode.classList.add('required-error');
+                  }
+                } else {
+                  if(field.getAttribute('data-required-label')) {
+                    form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
+                  } else {
+                    field.parentNode.classList.remove('required-error');
+                  }
+                }
+
+                field.addEventListener('change', function(e) {
+                  if(this.value === '') {
+                    form_error = true;
+                    if(this.getAttribute('data-required-label')) {
+                      form.querySelector('.'+this.getAttribute('data-required-label')).classList.remove('hide');
+                    } else {
+                      this.parentNode.classList.add('required-error');
+                    }
+                  } else {
+                    if(this.getAttribute('data-required-label')) {
+                      form.querySelector('.'+this.getAttribute('data-required-label')).classList.add('hide');
+                    } else {
+                      this.parentNode.classList.remove('required-error');
+                    }
+                  }
+                });
+              }
+            } else if(field.classList.contains('required-sum')) {
+              var ranges = field.querySelectorAll('input[type=range]'),
+                  field_total = 0;
+
+              Array.prototype.forEach.call(ranges, function(range) {
+                field_total += parseInt(range.value);
+              });
+
+              if(field_total !== 100) {
+                form_error = true;
+                form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
+              } else {
+                form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
+              }
+            } else if(field.classList.contains('radios') || field.nodeName === 'UL' || field.classList.contains('required-radios')) {
+              var skip = false;
+
+              if(field.hasAttribute('data-required-visible')) {
+                if(field.offsetWidth > 0 && field.offsetHeight > 0) {
+                  // is visible
+                } else {
                   skip = true;
                 }
               }
-            }
 
-            if(skip === false) {
-              if(field.value === '') {
-                form_error = true;
-                if(field.getAttribute('data-required-label')) {
-                  form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
-                } else {
-                  field.parentNode.classList.add('required-error');
-                }
-              } else {
-                if(field.getAttribute('data-required-label')) {
-                  form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
-                } else {
-                  field.parentNode.classList.remove('required-error');
-                }
-              }
+              if(skip === false) {
+                var field_checked_inputs = field.querySelectorAll('input:checked');
 
-              field.addEventListener('change', function(e) {
-                if(this.value === '') {
+                if(field_checked_inputs.length === 0) {
                   form_error = true;
-                  if(this.getAttribute('data-required-label')) {
-                    form.querySelector('.'+this.getAttribute('data-required-label')).classList.remove('hide');
+                  if(field.getAttribute('data-required-label')) {
+                    form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
                   } else {
-                    this.parentNode.classList.add('required-error');
+                    field.previousElementSibling.classList.add('required-error');
                   }
                 } else {
-                  if(this.getAttribute('data-required-label')) {
-                    form.querySelector('.'+this.getAttribute('data-required-label')).classList.add('hide');
+                  if(field.getAttribute('data-required-label')) {
+                    form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
                   } else {
-                    this.parentNode.classList.remove('required-error');
+                    field.previousElementSibling.classList.remove('required-error');
                   }
-                }
-              });
-            }
-          } else if(field.classList.contains('required-sum')) {
-            var ranges = field.querySelectorAll('input[type=range]'),
-                field_total = 0;
-
-            Array.prototype.forEach.call(ranges, function(range) {
-              field_total += parseInt(range.value);
-            });
-
-            if(field_total !== 100) {
-              form_error = true;
-              form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
-            } else {
-              form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
-            }
-          } else if(field.classList.contains('radios') || field.nodeName === 'UL' || field.classList.contains('required-radios')) {
-            var skip = false;
-
-            if(field.hasAttribute('data-required-visible')) {
-              if(field.offsetWidth > 0 && field.offsetHeight > 0) {
-                // is visible
-              } else {
-                skip = true;
-              }
-            }
-
-            if(skip === false) {
-              var field_checked_inputs = field.querySelectorAll('input:checked');
-
-              if(field_checked_inputs.length === 0) {
-                form_error = true;
-                if(field.getAttribute('data-required-label')) {
-                  form.querySelector('.'+field.getAttribute('data-required-label')).classList.remove('hide');
-                } else {
-                  field.previousElementSibling.classList.add('required-error');
-                }
-              } else {
-                if(field.getAttribute('data-required-label')) {
-                  form.querySelector('.'+field.getAttribute('data-required-label')).classList.add('hide');
-                } else {
-                  field.previousElementSibling.classList.remove('required-error');
                 }
               }
             }
+          });
+          
+          // Reset form submit
+          form_submitted = form_submit = null;
+
+          if(form_error === false) {
+            // e.preventDefault();
+            // return false;
+            return true;
+          } else {
+            e.preventDefault();
+            return false;
           }
-        });
-
-        // Reset form submit
-        form_submited = form_submit = null;
-
-        if(form_error === false) {
-          // e.preventDefault();
-          // return false;
-          return true;
-        } else {
-          e.preventDefault();
-          return false;
         }
-      }
-    });
+      });
+    }
   });
 
   function input_check(form, input) {
